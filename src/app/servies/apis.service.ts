184719e8b8425773502b73d1e033052f9b21d14c
abdulbasitchippa <abdulbasitchippa4@gmail.com>
2022-02-10 02:05:50 +0500

    profile page added

--- src/app/servies/apis.service.ts
+++ src/app/servies/apis.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
-import {HttpClient ,HttpClientModule,HttpResponse,  HttpHeaders, HttpErrorResponse } from '@angular/common/http';
-import { Observable, throwError } from 'rxjs';
+import {HttpClient ,HttpClientModule, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
+import {  throwError } from 'rxjs';
 import { retry, catchError } from 'rxjs/operators';
 
 
@@ -9,8 +9,8 @@ import { retry, catchError } from 'rxjs/operators';
 })
 export class ApisService {
 
-  // getApiUrl : string = "https://jsonplaceholder.typicode.com/posts";
   getApiUrl : string = "https://randomuser.me/api/?page=1&seed=feed";
+  getUserApiUrl : string = "https://randomuser.me/api/?page=1&seed=feed";
 
   constructor(private httpCM : HttpClientModule,private http: HttpClient) { }
 
@@ -22,22 +22,25 @@ export class ApisService {
 
   handleError(error: HttpErrorResponse) {
     if (error.error instanceof ErrorEvent) {
-      // A client-side or network error occurred. Handle it accordingly.
       console.error('An error occurred:', error.error.message);
     } else {
-      // The backend returned an unsuccessful response code.
-      // The response body may contain clues as to what went wrong,
       console.error(
         `Backend returned code ${error.status}, ` +
         `body was: ${error.error}`);
     }
-    // return an observable with a user-facing error message
     return throwError(
       'Something bad happened; please try again later.');
   };
 
   getPosts(postNo) {
     return this.http.get(this.getApiUrl+`&results=${postNo}`)
+      .pipe(
+        retry(2),
+        catchError(this.handleError)
+      )
+  }
+  getuser(uid) {
+    return this.http.get(this.getApiUrl+`&results=${uid}`)
       .pipe(
         retry(2),
         catchError(this.handleError)
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -10,69 +10,36 @@ import { IonInfiniteScroll } from '@ionic/angular';
 export class Tab1Page {
   @ViewChild(IonInfiniteScroll) infiniteScroll: IonInfiniteScroll;
 
-
   postsData
   postsDataAll
-
   postNo = 1
 
   constructor(public api: ApisService) {}
 
   loadData(event) {
     setTimeout(() => {
-
-      // App logic to determine if all data is loaded
-      // and disable the infinite scroll
-      // if (this.postsDataAll.length == 50 || this.postNo == 50) {
-      //   console.log("first if")
-      //   event.target.disabled = true;
-      //   console.log('Done');
-      //   event.target.complete();
-      // }
-      if(this.postsDataAll.length <= 5 || this.postNo <= 5){
-        this.postNo = this.postNo + 2
+      this.postNo = this.postNo + 3
         console.log("second if" , this.postNo)
         this.getPostData(this.postNo)
-
-        if(this.postNo <= 5){
-          console.log('Done form second if if');
           event.target.complete();
-          event.target.disabled = true;
-        }
-      }else{
-        console.log('Done');
-        event.target.complete();
-        console.log("second if else", this.postNo)
-        event.target.disabled = true;
-      }
     }, 500);
   }
 
-  // toggleInfiniteScroll() {
-  //   this.infiniteScroll.disabled = !this.infiniteScroll.disabled;
-  // }
 
   ionViewWillEnter() {
-    if (this.postNo <= 5) {
-      console.log("ionview", this.postNo)
-    this.getPostData(this.postNo)
-    }
+    console.log("ionview", this.postNo)
+    this.getPostData(this.postNo);
   }
 
-
   getPostData(postNo){
-
-    if(postNo <= 5){
+    if(postNo <= 10){
       this.api.getPosts(postNo).subscribe(response => {
         this.postsData = response;
-       //  console.log(this.postsData.results);
        this.postsDataAll = this.postsData.results
        console.log(this.postsDataAll);
      })
     }
-
   }
-
 
   getimage(id){
     console.log(id)
